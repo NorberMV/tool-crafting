38d7dd496b6c7500091cf8e04dc7baf7e122f731
norberMV <norbermv.dev@gmail.com>
2024-09-19 22:45:12 -0500

    Adding more logs for debug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain_core.utils.function_calling import convert_to_openai_function
 
 from tools import get_current_temperature
-from routing import route
+from routing import route, display_openai_func
 
 
 functions = [convert_to_openai_function(f) for f in [get_current_temperature]]
@@ -24,7 +24,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-chain = prompt | model | OpenAIFunctionsAgentOutputParser() | route
+chain = prompt | model | display_openai_func | OpenAIFunctionsAgentOutputParser() | route
 
 
 # Entry point
@@ -38,16 +38,18 @@ if __name__ == "__main__":
     print("* The tool arguments are:")
     print(get_current_temperature.args)
     print()
-    print("The name of the tool converted to OpenAIFunction is:")
+    print("The full object from the tool converted to OpenAIFunction is:")
     print(f"{functions[0]!r}")
     print()
     user_question = (
         "What is the weather in Lions Bay British Columbia Canada right now?"
     )
     # user_question = "What is the best book on OOP for a Python programmer?"
+
+    result = chain.invoke({"input": user_question})
+    print()
     print(
         f"* And finally, the answer from the tool to the question '{user_question}' is:"
     )
 
-    result = chain.invoke({"input": user_question})
     print(result)
